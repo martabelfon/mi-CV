44e33827337c9c0da1ac1902c4b5616b9af523f5
martabelfon <martabelfon@gmail.com>
2023-02-08 12:01:43 +0100

    fix: modificaciones de css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { CV } from './CV/CV';
 import './App.scss';
 import Navbar from './components/NavBar';
+import { Route, Routes } from 'react-router';
 
 console.log(CV);
 
@@ -9,7 +10,17 @@ function App() {
     <div className="App">
         <h1>Mi CV</h1>
         <Navbar/>
-        <div>Contenido..</div>
+        
+      <div className='container'>
+        <Routes>
+          <Route active path="/" element={<div>Inicio...</div>}/>
+          <Route path="/experience" element={<div>Experiencia...</div>}/>
+          <Route path="/education" element={<div>educacion...</div>}/>
+          <Route path="/languages" element={<div>lenguajes...</div>}/>
+          <Route path="/skills" element={<div>skills...</div>}/>
+        </Routes>
+      </div>
+
     </div>
   );
 }
